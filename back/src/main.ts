03d814fe800401d1199b9d7756c8dc3481546da9
RodrigoFernandez <rodd_2610@hotmail.com>
2025-02-18 23:03:01 -0300

    datitos

--- back/src/main.ts
+++ back/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors()
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 3001;
   await app.listen(PORT);
   console.log(`server lisent in port ${PORT}`);
 }
