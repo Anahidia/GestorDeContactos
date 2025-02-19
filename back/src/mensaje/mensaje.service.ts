77dc929be48b5c2aaae6de2cf6e7fe93a3c46637
Anahi Diaz <ad955180@gmail.com>
2025-02-18 23:33:01 -0300

    messages

--- back/src/mensaje/mensaje.service.ts
+++ back/src/mensaje/mensaje.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { MensajeRepository } from './mensaje.repository';
+import { CreateMessageDto } from './dto/createMensaje.dto';
 
 @Injectable()
-export class MensajeService {}
+export class MensajeService {
+    constructor(
+        private readonly mensajeRepository: MensajeRepository
+    ) {}
+
+    async getMensajesService() {
+        return await this.mensajeRepository.getAllMessages();
+    }
+    async getMensajeServiceById(id: string) {
+        return await this.mensajeRepository.getMessageById(id);
+    }
+
+    async createMensajeService(mensaje:CreateMessageDto) {
+        return await this.mensajeRepository.createMessage(mensaje);
+    }
+    async updateMensajeService(id: string, data: any) {
+        return await this.mensajeRepository.updateMessage(id, data);
+    }
+    async deleteMensajeService(id: string) {
+        return await this.mensajeRepository.deleteMessage(id);
+    }
+    async getMensajeServiceByAsunto(asunto: string) {
+        return await this.mensajeRepository.getMessageByAsunto(asunto);
+    }
+    
+}
